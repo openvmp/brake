c824ee49358f839ef7c53c6957f4e71934d26bf9
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-22 16:38:58 -0700

    Changing QoS settings to enable intraprocess communications

--- src/interface.cpp
+++ src/interface.cpp
@@ -44,7 +44,7 @@ Interface::Interface(rclcpp::Node *node, bool engaged_by_default)
       .depth = 1,
       .reliability =
           rmw_qos_reliability_policy_t::RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-      .durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+      .durability = RMW_QOS_POLICY_DURABILITY_VOLATILE,
       .deadline = {0, 50000000},
       .lifespan = {0, 50000000},
       .liveliness = RMW_QOS_POLICY_LIVELINESS_AUTOMATIC,
